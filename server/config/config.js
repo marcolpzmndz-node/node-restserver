9f124d5ce9264a7f500ae871b78f0c447422275b
Marco <marcolpzmndz@gmail.com>
2019-08-27 17:39:51 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,9 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://marcolpzmndz:VhRUFk42fzlCBbRM@cluster0-4katf.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
+
+    //mongodb+srv://marcolpzmndz:VhRUFk42fzlCBbRM@cluster0-4katf.mongodb.net/cafe;
 }
 
 process.env.URLDB = urlDB;
