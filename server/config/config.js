3c00ce7324a1df299f1ea7e752f75f3f74cb3d1f
Marco <marcolpzmndz@gmail.com>
2019-08-27 17:22:32 -0500

    Restserver - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,26 @@
 // ============================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//           ENTORNO
+// ============================
+
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//        BASE DE DATOS
+// ============================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://marcolpzmndz:VhRUFk42fzlCBbRM@cluster0-4katf.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
